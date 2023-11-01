d9ee17216035df70453b26c3214376bb268e0339
Corbian Reiser <corb.reiser@gmail.com>
2023-11-01 11:22:55 +0100

    add fib rec

--- C_Grundlagen/fibonacci.c
+++ C_Grundlagen/fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int fib(int n){
+int fib_iter(int n){
     int fib_one = 0;
     int fib_two = 1;
     for (int i = 0; i<n; i++){
@@ -11,11 +11,25 @@ int fib(int n){
     return fib_one;
 }
 
+int fib_rec(int n){
+    if (n <= 1){
+        return n;
+    }
+    else {
+        return fib_rec(n - 2) + fib_rec(n -1);
+    }
+}
+
 //Test
 void main(){
-    printf("%i\n", fib(30));
-    printf("%i\n", fib(0));
-    printf("%i\n", fib(1));
-    printf("%i\n", fib(4));
-    printf("%i\n", fib(10));
+    printf("%i\n", fib_iter(30));
+    printf("%i\n", fib_iter(0));
+    printf("%i\n", fib_iter(1));
+    printf("%i\n", fib_iter(4));
+    printf("%i\n", fib_iter(10));
+    printf("%i\n", fib_rec(30));
+    printf("%i\n", fib_rec(0));
+    printf("%i\n", fib_rec(1));
+    printf("%i\n", fib_rec(4));
+    printf("%i\n", fib_rec(10));
 }
